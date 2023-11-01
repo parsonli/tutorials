2f72e0174643e053e07e5dd773b9bb68d9631200
parsonli <50194295+parsonli@users.noreply.github.com>
2023-11-01 13:29:12 +0800

    1.3 todo component 1.4 props validation

---
+++ owl_playground/static/src/todo/todo.js
@@ -0,0 +1,13 @@
+/** @odoo-module **/
+
+import { Component } from "@odoo/owl";
+
+export class Todo extends Component {
+    static template = "owl_playground.todo";
+    static props = {
+        id: { type: Number },
+        description: { type: String },
+        done: { type: Boolean },
+    };
+
+}
